57f65aa22f2a4e19d92eeb602d66fac3065e39fb
Ivan Martinović <martinovic1412@gmail.com>
2022-01-18 11:59:56 +0100

    Initial Commit

---
+++ OpenCVTutTry06/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenCVTutTry06
+//
+//  Created by Ivan Martinović on 18.01.2022..
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
